d9b1d4ead7719601e2d951587548fe72ba3cbfbd
Saha-Ovi <ovisaha3210@gmail.com>
2026-05-09 12:13:59 +0600

    news details added

--- src/components/shared/NewsCard.jsx
+++ src/components/shared/NewsCard.jsx
@@ -1,23 +1,24 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { CiBookmark, CiShare2 } from 'react-icons/ci';
 import { FaEye } from 'react-icons/fa';
 import { IoIosStar } from 'react-icons/io';
 
-const NewsCard = ({news}) => {
+const NewsCard = ({ news }) => {
     return (
         <div>
             <div className="card bg-base-100 shadow-sm">
-             
+
                 <div className="card-body">
                     <div className='flex justify-between items-center'>
                         {/* left */}
                         <div className='flex items-center gap-2'>
-                        <Image className='rounded-full' src={news.author?.img} width={40} height={40} alt={news.author.name}></Image>
-                        <div>
-                         <p>{news.author?.name}</p>
-                         <p>{news.author?.published_date}</p>
-                        </div>
+                            <Image className='rounded-full' src={news.author?.img} width={40} height={40} alt={news.author?.name}></Image>
+                            <div>
+                                <p>{news.author?.name}</p>
+                                <p>{news.author?.published_date}</p>
+                            </div>
                         </div>
                         {/* right */}
                         <div className='flex items-center gap-2 text-2xl'>
@@ -41,7 +42,9 @@ const NewsCard = ({news}) => {
                                 <p>{news.total_view} K</p>
                             </div>
                         </div>
-                        <button href={`/news/${news._id}`}className="btn btn-base">Read More</button>
+                        <Link href={`/news/${news._id}`}>
+                            <button  className="btn btn-base">Read More</button>
+                        </Link>
                     </div>
                 </div>
             </div>
